28ee3b0954e22e514631ff174d166a621a07a781
Tim Winkelmann <twinkelmann@protonmail.com>
2022-07-01 19:33:42 +0200

    Initial commit

---
+++ copy-binary.js
@@ -0,0 +1,16 @@
+const { copyFileSync, mkdirSync } = require("fs");
+
+const arch = process.arch;
+const version = require("./package.json").dependencies["better-sqlite3"];
+console.log("Current architecture:", arch);
+console.log("Current better-sqlite3 version:", version);
+
+const inFile =
+  __dirname + "/node_modules/better-sqlite3/build/Release/better_sqlite3.node";
+const outFolder = `${__dirname}/bin/${version}/${arch}`;
+const outFile = `${outFolder}/better_sqlite3.node`;
+
+mkdirSync(outFolder, { recursive: true });
+console.log("Created output folder");
+copyFileSync(inFile, outFile);
+console.log("Copied binary file");
